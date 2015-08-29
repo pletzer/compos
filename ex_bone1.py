5425eaf2f46381ef3c92b499c7c1fd518874db43
ALEXANDER PLETZER <axp5530@psu.edu>
2015-08-29 19:44:59 -0400

    More interesting geometry

---
+++ ex_bone1.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+
+import compos2d
+import sys
+import math
+
+
+cs = compos2d.Compos2d()
+sys.path.append('examples')
+
+# input data
+import bone1
+pts = bone1.xy
+cs.setReference(pts)
+cs.plot(refFlag=True, var='rho')
+
+# modify reference
+n = len(pts)
+spcPts = [ (pts[i][0] + 0.10*math.sin(2*i*2*math.pi/float(n)),
+            pts[i][1] + 0.05*math.cos(2*i*2*math.pi/float(n)))
+                for i in range(n)]
+cs.setSpecimen(spcPts)
+cs.plot(refFlag=False, var='rho')
+
+refPos = (0., 1.5)
+spcPos, error, iter = cs.findSpecimenPoint(refPos, tol=1.e-6, niter=10, h=0.01)
+print 'refPos = {} spcPos = {} error = {} iter = {}'.format(refPos, spcPos, error, iter)
